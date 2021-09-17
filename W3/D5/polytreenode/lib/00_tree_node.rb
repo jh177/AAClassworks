c7a58eb7ba11a47bb2a7bf46c2bb2c38b41c852b
jh177 <jameshong177@gmail.com>
2021-09-17 15:19:42 -0700

    Add methods of initialize and parent=.

--- W3/D5/polytreenode/lib/00_tree_node.rb
+++ W3/D5/polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,27 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent=(node)
+    if !self.parent.nil?
+      @parent.children.delete(self)
+    end
+    @parent = node
+    if !node.nil? && !node.children.include?(self)
+      node.children << self
+    end
+  end
+
 end
+
+# p node_1 = PolyTreeNode.new(1)
+# p node_2 = PolyTreeNode.new(2)
+# p node_1.children << node_2
+# p node_2.parent= node_1
