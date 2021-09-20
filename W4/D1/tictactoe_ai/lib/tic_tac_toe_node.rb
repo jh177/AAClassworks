0fbbd29b85cf846485e1d34a5adf080eb581bb2e
jh177 <jameshong177@gmail.com>
2021-09-20 15:29:15 -0700

    Create initialize and children method for ttt_node

--- W4/D1/tictactoe_ai/lib/tic_tac_toe_node.rb
+++ W4/D1/tictactoe_ai/lib/tic_tac_toe_node.rb
@@ -1,7 +1,21 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def switch_mark
+    if @next_mover_mark == :x
+      @next_mover_mark = :o 
+    else
+      @next_mover_mark = :x
+    end
   end
 
   def losing_node?(evaluator)
@@ -13,5 +27,27 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    result = []
+    self.switch_mark
+    (0...3).each do |row|
+      (0...3).each do |col|
+        if @board[[row, col]].nil?
+          dupped_board = @board.dup
+          dupped_board[[row, col]] = @next_mover_mark
+          result << TicTacToeNode.new(dupped_board, @next_mover_mark, [row, col])
+        end
+      end
+    end
+    result
   end
+
+  # def inspect
+  #   {"board" => @board, "prev_move" => @prev_move_pos}
+  # end
+
+
+
+ 
+  
+  
 end
