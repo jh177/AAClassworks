a93dfb7ea2307a305ba5ad84d885c55f70a8bba7
Alan Chiu <ms0372631@gmail.com>
2021-09-22 11:59:54 -0700

    populate method

--- W4/D2/chess/board.rb
+++ W4/D2/chess/board.rb
@@ -1,5 +1,13 @@
 
 require_relative "./pieces/piece.rb"
+require_relative "./pieces/bishop.rb"
+require_relative "./pieces/king.rb"
+require_relative "./pieces/knight.rb"
+require_relative "./pieces/nullpiece.rb"
+require_relative "./pieces/pawn.rb"
+require_relative "./pieces/queen.rb"
+require_relative "./pieces/rook.rb"
+
 
 class Board
 
@@ -7,8 +15,7 @@ class Board
 
   def initialize
     @rows = Array.new(8) {Array.new(8)}
-    set_pos = [0, 1].product((0..7).to_a) + [6, 7].product((0..7).to_a)
-    set_pos.each {|pos| @rows[pos[0]][pos[1]] = Piece.new }
+    populate
   end
 
   def [](pos)
@@ -56,7 +63,38 @@ class Board
   end
 
 
+    def populate
+      (0..7).each do |j|
+        @rows[1][j] = Pawn.new("black", self, [1, j])
+        @rows[6][j] = Pawn.new("white", self, [6, j])
+      end
 
+      [2,3,4,5].product((0..7).to_a).each do |pos|
+        r, c = pos
+        @rows[r][c] = Nullpiece.new
+      end
+      
+      @rows[0][0] = Rook.new("black", self, [0,0])
+      @rows[0][7] = Rook.new("black", self, [0,7])
+      @rows[7][0] = Rook.new("white", self, [7,0])
+      @rows[7][7] = Rook.new("white", self, [7,7])
 
+      @rows[0][1] = Knight.new("black", self, [0,1])
+      @rows[0][6] = Kngiht.new("black", self, [0,6])
+      @rows[7][1] = Knight.new("white", self, [7,1])
+      @rows[7][6] = Knight.new("white", self, [7,6])
+
+      @rows[0][2] = Bishop.new("black", self, [0,2])
+      @rows[0][5] = Bishop.new("black", self, [0,5])
+      @rows[7][2] = Bishop.new("white", self, [7,2])
+      @rows[7][5] = Bishop.new("white", self, [7,5])
+
+      @rows[0][3] = Queen.new("black", self, [0,3])
+      @rows[7][3] = Queen.new("white", self, [7,3])
+
+      @rows[0][4] = King.new("black", self, [0,4])
+      @rows[7][4] = King.new("white", self, [7,4])
+
+    end
 
 end
