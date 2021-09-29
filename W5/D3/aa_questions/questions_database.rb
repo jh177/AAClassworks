de6e3e06c343f5c1d6209fc53883bf0ea040ddb6
jh177 <jameshong177@gmail.com>
2021-09-29 13:35:25 -0700

    Create questions_database ruby file and gemfile

---
+++ W5/D3/aa_questions/questions_database.rb
@@ -0,0 +1,25 @@
+require "sqlite3"
+require "singleton"
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super("questions.db")  ##??
+    # self.type_translation = true
+    # self.results_as_hash = true
+  end
+
+end
+
+class Question
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")  ##??
+    data.map { |datum| Question.new(datum) }
+  end
+
+
+
+
+end
