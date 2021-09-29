91d668df46433df9dbc9e415ba9c8459f44d2cd3
Grayson Poon <grayson.a.poon@gmail.com>
2021-09-29 12:02:47 -0700

    Finish seeding values into tables

--- W5/D3/aa_questions/import_db.sql
+++ W5/D3/aa_questions/import_db.sql
@@ -2,20 +2,24 @@ PRAGMA foreign_keys = ON;
 
 --replies - questions likes -- questions follows -- questions -- users
 
-DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
 
-CREATE TABLE replies (
+CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
   body TEXT NOT NULL,
-  question_id INTEGER NOT NULL,
   author_id INTEGER NOT NULL,
-  root_id INTEGER NOT NULL,
 
-  FOREIGN KEY question_id REFERENCES questions(id),
-  FOREIGN KEY author_id REFERENCES users(id),
-  FOREIGN KEY root_id REFERENCES replies(id)
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
 
 DROP TABLE IF EXISTS question_follows;
 
@@ -32,11 +36,9 @@ CREATE TABLE question_follows (
   follower_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
 
-  FOREIGN KEY follower_id REFERENCES users(id),
-  FOREIGN KEY question_id REFERENCES questions(id)
+  FOREIGN KEY (follower_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 );
-
-
 
 DROP TABLE IF EXISTS question_likes;
 
@@ -45,31 +47,23 @@ CREATE TABLE question_likes(
   user INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
 
-  FOREIGN KEY user REFERENCES users(id),
-  FOREIGN KEY question_id REFERENCES questions(id)
+  FOREIGN KEY (user) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
+DROP TABLE IF EXISTS replies;
 
-DROP TABLE IF EXISTS questions;
-
-CREATE TABLE questions (
+CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
-  title TEXT NOT NULL,
   body TEXT NOT NULL,
+  question_id INTEGER NOT NULL,
   author_id INTEGER NOT NULL,
+  parent_id INTEGER,
 
-  FOREIGN KEY author_id REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
 );
-
-
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  fname TEXT NOT NULL,
-  lname TEXT NOT NULL
-);
-
 
 ----------------------------
 
@@ -83,15 +77,28 @@ VALUES
 INSERT INTO
   questions (title, body, author_id)
 VALUES
-  ('How to code?', 'body...', (SELECT id FROM users WHERE fname = 'Steve' AND lname = 'Smith')),
-  ('How to whiteboard?', 'body...', (SELECT id FROM users WHERE fname = 'Andy' AND lname = 'Huang'));
+  ('How to code?', 'body...', 1),
+  ('How to whiteboard?', 'body...', 2);
 
 
---replies - questions likes -- questions follows (follower_id, quesiton_id)
+--replies - questions likes (user, question_id) -- questions follows (follower_id, quesiton_id)
 INSERT INTO
   question_follows (follower_id, question_id)
 VALUES
-  ((SELECT id FROM users WHERE fname = 'Steve' AND lname = 'Smith'), (SELECT id FROM questions WHERE title = 'How to code?') ),
-  ((SELECT id FROM users WHERE fname = 'Andy' AND lname = 'Huang'), (SELECT id FROM questions WHERE title = 'How to whiteboard?') );
+  (1, 1),
+  (2, 1);
+
+INSERT INTO
+  question_likes (user, question_id)
+VALUES
+  (1, 1),
+  (2, 2);
+
+-- INSERT INTO
+--   replies (body, question_id, author_id, parent_id)
+-- VALUES
+--   ('Go to aA!', 1, 3, NULL),
+--   ('Just do it', 2, 2, NULL);
+
 
 
