ab4c71c003cf5e0fe9b812a7951b8c1e7e213276
jh177 <jameshong177@gmail.com>
2021-09-30 16:19:14 -0700

    Complete association_excercise project

--- W5/D4/associations_exercise/app/models/course.rb
+++ W5/D4/associations_exercise/app/models/course.rb
@@ -15,4 +15,17 @@ class Course < ApplicationRecord
     primary_key: :id,
     foreign_key: :course_id,
     class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments, source: :user
+
+  belongs_to :prereq,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+
+  has_one :instructor,
+    primary_key: :instructor_id,
+    foreign_key: :id,
+    class_name: :User
 end
