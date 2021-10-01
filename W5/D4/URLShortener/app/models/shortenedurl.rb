81316ad5c9a26d663bddee9a64d56c0a26d25691
jh177 <jameshong177@gmail.com>
2021-09-30 17:54:24 -0700

    Migrate create tables for user and shortened_url

---
+++ W5/D4/URLShortener/app/models/shortenedurl.rb
@@ -0,0 +1,3 @@
+class ShortenedUrl < ApplicationRecord
+  
+end
--- W5/D4/URLShortener/db/migrate/20211001003706_create_shortened_urls.rb
+++ W5/D4/URLShortener/db/migrate/20211001003706_create_shortened_urls.rb
@@ -3,7 +3,9 @@ class CreateShortenedUrls < ActiveRecord::Migration[5.2]
     create_table :shortened_urls do |t|
       t.string :long_url, null: false
       t.string :short_url, null: false
+      t.integer :user_id, null: false
       t.timestamps
     end
+    add_index :shortened_urls, [:long_url, :short_url], unique: true
   end
 end
