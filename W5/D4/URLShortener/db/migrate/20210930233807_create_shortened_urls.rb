5852da1ec5d65bb84cdb5509ffc363eed69bebb2
jh177 <jameshong177@gmail.com>
2021-09-30 17:16:36 -0700

    Delete URL project folder

--- W5/D4/URLShortener/db/migrate/20210930233807_create_shortened_urls.rb
+++
@@ -1,9 +0,0 @@
-class CreateShortenedUrls < ActiveRecord::Migration[5.2]
-  def change
-    create_table :shortened_urls do |t|
-      t.string :long_url, null: false
-      t.string :short_url, null: false
-      t.timestamps
-    end
-  end
-end
