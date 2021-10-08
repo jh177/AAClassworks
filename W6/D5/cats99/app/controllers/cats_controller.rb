eca9da8355287931029752ebafc2d7727ed80831
jh177 <jameshong177@gmail.com>
2021-10-08 16:25:41 -0700

    Build index action and html.erb file for index

--- W6/D5/cats99/app/controllers/cats_controller.rb
+++ W6/D5/cats99/app/controllers/cats_controller.rb
@@ -1,5 +1,10 @@
 class CatsController < ApplicationController
 
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
   
 
 end
--- W6/D5/cats99/db/seeds.rb
+++ W6/D5/cats99/db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  Cat.destroy_all
+
+richard = Cat.create(
+  birth_date: "1992/02/18",
+  color: "black",
+  name: "Richard",
+  sex: "M",
+  description: "New cat"
+)
+
+aji = Cat.create(
+  birth_date: "2020/08/18",
+  color: "white",
+  name: "Aji",
+  sex: "F",
+  description: "actually a dog"
+)
+
+jared = Cat.create(
+  birth_date: "1998/05/22",
+  color: "brown",
+  name: "Jared",
+  sex: "M",
+  description: "COO"
+)
+
+aji = Cat.create(
+  birth_date: "2020/07/18",
+  color: "yellow",
+  name: "Lucki",
+  sex: "F",
+  description: "actually another dog"
+)
+
+end
