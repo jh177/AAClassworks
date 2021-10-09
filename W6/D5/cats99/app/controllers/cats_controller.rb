c40627cb100c90b35fa888f5b7e520820e8e15ac
Cal Brown <calsbrown4@gmail.com>
2021-10-08 17:00:50 -0700

    Final Edit

--- W6/D5/cats99/app/controllers/cats_controller.rb
+++ W6/D5/cats99/app/controllers/cats_controller.rb
@@ -5,6 +5,27 @@ class CatsController < ApplicationController
     render :index
   end
 
+  def show
+    @cat = Cat.find_by(id: params[:id])
+    render :show
+  end
   
+  def new
+    @cat = Cat.new
+    render :new
+  end
 
+  def create
+    new_cat = Cat.new(cat_params)
+
+    if new_cat.save
+      redirect_to cat_url(cat)
+      else
+        render :new
+      end
+  end
+
+  def cat_params
+    params.require(:cat).permit(:name,:birth_date,:color,:sex,:description)
+  end
 end
