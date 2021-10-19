f29b1ac13682221cabb0b022adde5da09ab6ef93
Emmett He <emmetthe26@gmail.com>
2021-10-19 14:45:41 -0700

    finish p1

--- W8/D2/intro_javascript_exercises/phase_1_arrays.js
+++ W8/D2/intro_javascript_exercises/phase_1_arrays.js
@@ -13,5 +13,33 @@ Array.prototype.uniq = function() {
 
 Array.prototype.twoSum = function() {
   let pairs = []
-  
+  for(let i = 0; i < this.length - 1; i++) {
+    for(let j = 1; j < this.length; j++) {
+      if ((i < j) && (this[i] + this[j] === 0)) {
+        pairs.push([i, j]);
+      }
+    }
+  }
+  return pairs;
 }
+
+// console.log([1, 2, 2, 3, 3, -3].twoSum())
+
+Array.prototype.transpose = function() {
+  let result = [];
+  for(let col = 0; col < this[0].length; col++) {
+    let subArray = [];
+    for(let row = 0; row < this.length; row++) {
+      subArray.push(this[row][col]);
+    }
+    result.push(subArray);
+  }
+  return result;
+}
+
+// console.log([
+//   [1, 2], 
+//   [2, 3], 
+//   [3]
+// ].transpose());
+
