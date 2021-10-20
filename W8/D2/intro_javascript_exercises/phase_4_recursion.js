3b073a9ab3d5deb5299e2fa7a5d7de2e731327a8
jh177 <jameshong177@gmail.com>
2021-10-19 17:31:20 -0700

    Complete phash_4 of W8D2 project

--- W8/D2/intro_javascript_exercises/phase_4_recursion.js
+++ W8/D2/intro_javascript_exercises/phase_4_recursion.js
@@ -22,3 +22,32 @@ function exp1(b, n) {
     return exp1(b, n + 1) / b;
   }
 }
+
+function exp2(b, n){
+  if (n === 0) {
+    return 1;
+  }
+  if (n%2===0) {
+    return exp2(b, n/2)**2;
+  } else {
+    return b*(exp2(b, (n-1)/2)**2);
+  }
+}
+
+function fibonacci(n) {
+  if (n<3) {
+    return [0,1];
+  }
+  let fibs = fibonacci(n-1);
+  fibs.push(fibs[fibs.length-1]+fibs[fibs.length-2]);
+  return fibs;
+}
+
+function deepDup(array) {
+  if (!array instanceof Array) {
+    return array;
+  }
+  return array.map((el) => 
+    deepDup(el)
+  );
+}
