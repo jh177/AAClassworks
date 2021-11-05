1ece45a43a4d967e1878fce0a9e264c5d92e2732
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-11-05 16:02:38 -0700

    setState not working

--- W10/D5/widgets/frontend/clock.jsx
+++ W10/D5/widgets/frontend/clock.jsx
@@ -1,5 +1,33 @@
+import React from 'react';
 
 class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+     time: new Date()
+    }
+    this.tick.bind(this);
+    this.setState.bind(this);
+  }
+
+  tick(){
+    this.setState({time: new Date()}); 
+  }
+
+  componentDidMount(){
+    //Function is run after component renders intially
+    //Use setinterval to call tick every second 
+    setInterval(this.tick, 1000);
+  }
+
+  render(){
+    return (
+     <div> 
+       <h1>Clock </h1>
+        {this.state.time.toLocaleString()}
+     </div>
+    );
+  }
 
 }
 
--- W10/D5/widgets/widget.jsx
+++ W10/D5/widgets/widget.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Clock from './frontend/clock'; 
 
 window.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<div id="root"/>, document.getElementById("main"));
+  ReactDOM.render(<Clock />, document.getElementById("root")); 
 })
