4d0d6991c6f032838d37eea15131b8ba015466e8
jh177 <jameshong177@gmail.com>
2021-11-05 15:13:53 -0700

    Setup widget project for W10D5

---
+++ W10/D5/widgets/widget.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+window.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<div id="root"/>, document.getElementById("main"));
+})
