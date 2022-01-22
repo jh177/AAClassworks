f2fc58deb92ade295b749f8708b8cc3b6dd40af8
jh177 <jameshong177@gmail.com>
2022-01-21 22:37:09 -0800

    Add insertion

--- DSA/insertion_sort_project/lib/insertion_sort.js
+++ DSA/insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,15 @@
 function insertionSort(arr) {
+    for (let i = 1; i<arr.length; i++){
+        let current = arr[i];
+        let j = i-1
 
+        while (j>=0 && current < arr[j]){
+            arr[j+1] = arr[j]
+            j -= 1 
+        }
+        arr[j+1] = current
+    }
+    return arr
 }
 
 module.exports = {
