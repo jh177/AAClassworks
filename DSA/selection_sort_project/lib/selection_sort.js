8ff62eb52b775d26db38771837eda9abdfc895b8
jh177 <jameshong177@gmail.com>
2022-01-21 22:29:18 -0800

    Add selection sort

--- DSA/selection_sort_project/lib/selection_sort.js
+++ DSA/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,21 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
 }
 
 function selectionSort(arr) {
+    for (let i=0; i<arr.length; i++){
+        let smaller = i;
 
+        for (let j=i+1; j<arr.length; j++){
+            if (arr[smaller] > arr[j]){
+                smaller = j 
+            }
+        }
+
+        swap(arr, i, smaller)
+    }
 }
 
 module.exports = {
